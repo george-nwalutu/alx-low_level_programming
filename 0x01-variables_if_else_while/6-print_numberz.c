1b6c5f4c9f6e05bae22841957bb8ff64753243d7
george-nwalutu <gnwalutu@gmail.com>
2023-08-11 11:52:49 -0700

    base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints base 10 numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+
+	for (s = 48; s < 58; s++)
+	{
+		putchar(s);
+	}
+	putchar('\n');
+	return (0);
+}
