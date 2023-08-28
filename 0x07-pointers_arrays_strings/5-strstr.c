24b01c3cfd59747d5fce4f87c3784e6a314d3c82
george-nwalutu <gnwalutu@gmail.com>
2023-08-28 11:06:54 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - program that locates a substring
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *j = haystack;
+		char *p = needle;
+
+		while (*j == *p && *p != '\0')
+		{
+			j++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
