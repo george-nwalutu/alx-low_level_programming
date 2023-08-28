2fb9ffc6f213d54e74d4ff14310109e2abfa8516
george-nwalutu <gnwalutu@gmail.com>
2023-08-28 07:52:32 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_memcpy - program that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
+ *
+ *Return: copied memory with n bytes changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int s = 0;
+	int i = n;
+
+	for (; s < i; s++)
+	{
+		dest[s] = src[s];
+		n--;
+	}
+	return (dest);
+}
